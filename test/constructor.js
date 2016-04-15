01a7b6c09355f04f72df7f6878f0ee7ea5c9d8f0
wuchangming <wuchangmingnice@163.com>
2016-04-15 19:15:02 +0800

    [功能] 修改keyUsage范围

--- test/constructor.js
+++ test/constructor.js
@@ -1,4 +1,6 @@
 var Mitmproxy = require('../lib');
+// MitmProxy转发时信任任何证书
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 var proxy = new Mitmproxy({
     ssl: true,
     sslConnectInterceptor: function() {return true;}
