efcae4353c907636287ddc0913d2898917563e36
wuchangming <wuchangmingnice@163.com>
2018-09-07 13:47:14 +0800

    update path

--- example/config/caCertPathConfig.js
+++ example/config/caCertPathConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
     sslConnectInterceptor: (req, cltSocket, head) => true,
-    caCertPath: '/Users/wuchangming834/node-mitmproxy/bk/node-mitmproxy.ca.crt',
-    caKeyPath: '/Users/wuchangming834/node-mitmproxy/bk/node-mitmproxy.ca.key.pem'
+    caCertPath: '/Users/wuchangming/node-mitmproxy/bk/node-mitmproxy.ca.crt',
+    caKeyPath: '/Users/wuchangming/node-mitmproxy/bk/node-mitmproxy.ca.key.pem'
 }
